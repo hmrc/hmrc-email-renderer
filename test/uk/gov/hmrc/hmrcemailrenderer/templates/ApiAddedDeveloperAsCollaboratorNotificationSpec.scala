9ba037356bef2d26ef214352148575249913fbe5
Chris Oxley <ctoxley@tentoltd.co.uk>
2017-01-13 11:55:18 +0000

    APIS-1220 API add developer as collaborator notification. Dev hub title.

---
+++ test/uk/gov/hmrc/hmrcemailrenderer/templates/ApiAddedDeveloperAsCollaboratorNotificationSpec.scala
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.hmrcemailrenderer.templates
+
+import junit.framework.TestCase
+import org.scalatestplus.play.{OneAppPerTest, PlaySpec}
+
+class ApiAddedDeveloperAsCollaboratorNotificationSpec extends PlaySpec with OneAppPerTest {
+
+  val email = "email@email.com"
+  val applicationName = "Application Name"
+  val role = "role"
+  val developerHubTitle = "Developer Hub Title"
+
+  val templateParams = Map("email" -> email, "applicationName" -> applicationName, "role" -> role,
+                           "developerHubTitle" -> developerHubTitle,
+                           "staticAssetUrlPrefix" -> "http://uri", "staticAssetVersion" -> "v1")
+
+  "htmlView" must {
+    "render as" in new TestCase {
+      val renderedHtml = api.html.apiAddedDeveloperAsCollaboratorNotification.render(templateParams)
+      renderedHtml.contentType must include("text/html")
+      renderedHtml.body must include("<p style=\"margin: 0 0 30px; font-size: 19px;\"><strong>" + email +
+                                     "</strong> has been added to <strong>" + applicationName +
+                                     "</strong> by another admin.</p>")
+      renderedHtml.body must include("<p style=\"margin: 0 0 30px; font-size: 19px;\">They now have " + role +
+                                     " rights over the application.</p>")
+      renderedHtml.body must include("<p style=\"margin: 0 0 30px; font-size: 19px;\">From " + developerHubTitle)
+    }
+  }
+
+  "textView" must {
+    "render as" in new TestCase {
+      val renderedTxt = api.txt.apiAddedDeveloperAsCollaboratorNotification.render(templateParams)
+      renderedTxt.contentType must include("text/plain")
+      renderedTxt.body must include(email + " has been added to " + applicationName + " by another admin.")
+      renderedTxt.body must include("They now have " + role + " rights over the application.")
+      renderedTxt.body must include("From " + developerHubTitle)
+    }
+  }
+}
