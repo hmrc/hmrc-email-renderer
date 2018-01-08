b309ae5af5e40f11dc662c54a6bba2f2f9a6b72d
harshita-yeole <harshita.yeole@digital.hmrc.gov.uk>
2018-01-08 10:40:32 +0530

    GGPUNE-724: Add email templates for lost-credentials retrieve user ID

--- app/preview/TemplateParams.scala
+++ app/preview/TemplateParams.scala
@@ -1088,6 +1088,10 @@ object TemplateParams {
     "hts_verification_email" -> Map(
       "name" -> "Joe",
       "verificationLink" -> exampleLinkWithRandomId
+    ),
+    "retrieve_userid_notification" -> Map(
+      "user_id" -> "914657894302",
+      "full_name" -> "Joe Bloggs"
     )
   )
 }
---
+++ app/uk/gov/hmrc/hmrcemailrenderer/templates/lostcredentials/LostCredentialsTemplates.scala
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.hmrcemailrenderer.templates.lostcredentials
+
+import uk.gov.hmrc.hmrcemailrenderer.domain.{MessagePriority, MessageTemplate}
+import uk.gov.hmrc.hmrcemailrenderer.templates.FromAddress
+import uk.gov.hmrc.hmrcemailrenderer.templates.ServiceIdentifier.LostCredentials
+
+object LostCredentialsTemplates {
+  val templates: Seq[MessageTemplate] = Seq(MessageTemplate.create(
+    templateId = "retrieve_userid_notification",
+    fromAddress = FromAddress.noReply("Government Gateway service"),
+    service = LostCredentials,
+    subject = "Government Gateway User ID",
+    plainTemplate = txt.lostCredentialsRetrieveUseridNotificationEmail.f,
+    htmlTemplate = html.lostCredentialsRetrieveUseridNotificationEmail.f,
+    priority = Some(MessagePriority.Urgent)))
+
+}
