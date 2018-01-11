3f32985a0bfa893fcf3aa2a2f13d34b07b949907
Richard Dennehy <richard.dennehy@outlook.com>
2018-01-11 10:40:41 +0000

    SDIL-996 Sdil email templates for subscription received and subscription activated (#133)
    * SDIL-996 added the template for the subscription received notification

--- app/preview/TemplateParams.scala
+++ app/preview/TemplateParams.scala
@@ -1089,6 +1089,14 @@ object TemplateParams {
       "name" -> "Joe",
       "verificationLink" -> exampleLinkWithRandomId
     ),
+    "sdil_registration_accepted" -> Map(
+      "sdilNumber" -> "ZZSDIL0009999",
+      "companyName" -> "Some company Ltd."
+    ),
+    "sdil_registration_received" -> Map(
+      "companyName" -> "Some company Ltd.",
+      "sdilNumber" -> "ZZSDIL0009999"
+    ),
     "retrieve_userid_notification" -> Map(
       "user_id" -> "914657894302",
       "full_name" -> "Joe Bloggs"
