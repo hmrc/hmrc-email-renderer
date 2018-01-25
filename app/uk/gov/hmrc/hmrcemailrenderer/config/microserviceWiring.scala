f0158269a84ba832f78989ccd4362c028863900f
Tevyn Development <tevyn-development@Tevyns-MacBook-Pro.local>
2018-01-25 10:57:13 +0000

    [TA][DC-1309] Step 1 - Package name changes

--- app/uk/gov/hmrc/hmrcemailrenderer/config/microserviceWiring.scala
+++ app/uk/gov/hmrc/hmrcemailrenderer/config/microserviceWiring.scala
@@ -16,11 +16,11 @@
 
 package uk.gov.hmrc.hmrcemailrenderer
 
-import uk.gov.hmrc.play.audit.http.config.LoadAuditingConfig
+import uk.gov.hmrc.play.microservice.config.LoadAuditingConfig
 import uk.gov.hmrc.play.audit.http.connector.AuditConnector
 import uk.gov.hmrc.play.auth.microservice.connectors.AuthConnector
 import uk.gov.hmrc.play.config.{AppName, RunMode, ServicesConfig}
-import uk.gov.hmrc.play.http.hooks.HttpHook
+import uk.gov.hmrc.http.hooks.HttpHook
 import uk.gov.hmrc.play.http.ws._
 
 object WSHttp extends WSGet with WSPut with WSPost with WSDelete with WSPatch with AppName {
