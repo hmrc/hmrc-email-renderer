523c7833cea8a6150286ef2fa856cecb4d4746fc
matthewgroom <matthew.groom@ntlworld.com>
2018-05-31 16:34:42 +0100

    cds test templates added

---
+++ app/uk/gov/hmrc/hmrcemailrenderer/templates/cds/cdsTestTemplates.scala
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.hmrcemailrenderer.templates.cds
+
+import uk.gov.hmrc.hmrcemailrenderer.domain.{MessagePriority, MessageTemplate}
+import uk.gov.hmrc.hmrcemailrenderer.templates.FromAddress
+import uk.gov.hmrc.hmrcemailrenderer.templates.ServiceIdentifier.CDS
+
+object cdsTestTemplates {
+
+  val templates = Seq(
+    MessageTemplate.create(
+      templateId = "cdsTestTemplate",
+      fromAddress = FromAddress.noReply("CDS Test Email"),
+      service  = CDS,
+      subject = "CDS Test Email",
+      plainTemplate = txt.cdsTestTemplate.f,
+      htmlTemplate = html.cdsTestTemplate.f,
+      priority = Some(MessagePriority.Urgent))
+  )
+}
