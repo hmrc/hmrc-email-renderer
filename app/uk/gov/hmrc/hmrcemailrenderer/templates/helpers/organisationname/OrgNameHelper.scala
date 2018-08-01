7aab6e7dec04f4a3bcbf2923c6955abe83063ed6
julian.saunders <julian.saunders@digital.hmrc.gov.uk>
2018-08-01 11:56:05 +0100

    GYE-1019 Content for Confirmation Email

---
+++ app/uk/gov/hmrc/hmrcemailrenderer/templates/helpers/organisationname/OrgNameHelper.scala
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.hmrcemailrenderer.templates.helpers.organisationname
+
+object OrgNameHelper {
+
+  def capitalised(param: Option[Any]) =
+    param.map { value =>
+      val buffer = scala.collection.mutable.ArrayBuffer.empty[Char]
+      value.toString.copyToBuffer(buffer)
+      buffer.zipWithIndex.map { case (char, idx) =>
+        if (idx == 0 || !buffer(idx - 1).isLetter) char.toUpper
+        else char.toLower
+      }.mkString
+    }
+
+
+  def orgNameOrYou(params: Map[String, Any]): String = {
+    val orgNameParams: List[Option[String]] = List(
+      capitalised(params.getNonEmpty("recipientOrgName"))
+    )
+
+    orgNameParams match {
+      case List(Some(orgName)) => s"$orgName is"
+      case _ => "You are"
+    }
+  }
+
+  private implicit class ParamsOps(params: Map[String,Any]){
+    def getNonEmpty(key: String): Option[Any] = {
+      params.get(key) match {
+        case Some(value) if value.toString.trim.length > 0 => Some(value)
+        case _ => None
+      }
+    }
+  }
+}
---
+++ test/uk/gov/hmrc/hmrcemailrenderer/templates/helpers/organisationname/OrgNameHelperSpec.scala
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.hmrcemailrenderer.templates.helpers.organisationname
+
+import uk.gov.hmrc.hmrcemailrenderer.templates.helpers.organisationname.OrgNameHelper._
+import uk.gov.hmrc.play.test.UnitSpec
+
+class OrgNameHelperSpec extends UnitSpec {
+    "The orgName" should {
+
+      "be 'You are' when having no orgName" in {
+        orgNameOrYou(Map()) should be("You are")
+      }
+
+      "be 'Orgname is' when having orgName only" in {
+        orgNameOrYou(Map("recipientOrgName" -> "Orgname")) should be("Orgname is")
+      }
+
+      "be 'Orgname is' when having orgName in uppercase" in {
+        orgNameOrYou(Map("recipientOrgName" -> "ORGNAME")) should be("Orgname is")
+      }
+
+      "be 'Orgname is' when having orgName in lowercase" in {
+        orgNameOrYou(Map("recipientOrgName" -> "orgname")) should be("Orgname is")
+      }
+
+      "be 'Orgname-Orgname is' when having hyphenated orgName" in {
+        orgNameOrYou(Map("recipientOrgName" -> "orgname-orgname")) should be("Orgname-Orgname is")
+      }
+
+      "be respected when having apostrophied orgName" in {
+        orgNameOrYou(Map("recipientOrgName" -> "o'orgname")) should be("O'Orgname is")
+      }
+
+      "be 'Orgname Orgname is' when having orgName with spaces" in {
+        orgNameOrYou(Map("recipientOrgName" -> "orgname orgname")) should be("Orgname Orgname is")
+      }
+    }
+}
