b04683bb5decf74edc586f951595ec2dc02116b5
Jack Witchell <jack.witchell@digital.hmrc.gov.uk>
2018-10-29 15:44:07 +0000

    AIR597 email template

--- test/uk/gov/hmrc/hmrcemailrenderer/templates/TemplateLocatorSpec.scala
+++ test/uk/gov/hmrc/hmrcemailrenderer/templates/TemplateLocatorSpec.scala
@@ -178,6 +178,7 @@ class TemplateLocatorSpec extends UnitSpec with OneAppPerSuite {
         "lt100_return_confirmation",
         "gd95_return_confirmation",
         "gd94_return_confirmation",
+        "air597_return_confirmation",
         "fhdds_submission_confirmation",
         "fhdds_submission_withdrawal",
         "fhdds_email_verification",
