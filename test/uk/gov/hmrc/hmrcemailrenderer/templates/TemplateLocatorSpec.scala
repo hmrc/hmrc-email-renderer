66db4ee6020baa944263860bca4f074016e5e14d
vijay <vasantha.vijayakrishna@gmail.com>
2018-12-17 14:15:22 +0000

    Updated the template name

--- test/uk/gov/hmrc/hmrcemailrenderer/templates/TemplateLocatorSpec.scala
+++ test/uk/gov/hmrc/hmrcemailrenderer/templates/TemplateLocatorSpec.scala
@@ -417,7 +417,7 @@ class TemplateLocatorSpec extends UnitSpec with OneAppPerSuite {
         "pods_psa_register",
         "pods_psa_invited",
         "vat",
-        "2WSM-question",
+        "newMessageAlert_2WSM-question",
         "2wsm-advisor"
         )
     }
