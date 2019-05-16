d9709989c8e41cb1c126dfe0e7f668c23c665943
Jonathan-Courage <48124229+Jonathan-Courage@users.noreply.github.com>
2019-05-16 15:07:54 +0100

    content update to CET template and creation of GSS template (#353)

--- test/uk/gov/hmrc/hmrcemailrenderer/templates/TemplateLocatorSpec.scala
+++ test/uk/gov/hmrc/hmrcemailrenderer/templates/TemplateLocatorSpec.scala
@@ -215,6 +215,7 @@ class TemplateLocatorSpec extends UnitSpec with OneAppPerSuite {
         "rdec_email_confirmation",
         "csr_submission_confirmation",
         "cet_email_confirmation",
+        "gss_email_confirmation",
         "cash_declaration",
         "register_for_duty_on_gas_for_road_fuel_use",
         "register_for_duty_on_biofuels_and_other_fuel_substitutes",
