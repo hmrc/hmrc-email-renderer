ca37ca1affeccfa1baeb93016a5127a7062bef68
Ali Karami <alijy@users.noreply.github.com>
2019-07-29 11:09:56 +0100

    DL-2173 - Welsh DFS templates added (#379)
    * DL-2173 - welsh DFS templates added
    
    * DL-2173 - Welsh DFS templates added

--- app/uk/gov/hmrc/hmrcemailrenderer/templates/dfs/DfsTemplates.scala
+++ app/uk/gov/hmrc/hmrcemailrenderer/templates/dfs/DfsTemplates.scala
@@ -33,12 +33,30 @@ object DfsTemplates {
       priority = Some(MessagePriority.Urgent)
     ),
     MessageTemplate.create(
+      templateId = "dfs_submission_success_r39_2015_welsh",
+      fromAddress = govUkTeamAddress,
+      service = DigitalFormsService,
+      subject = "R39: Claim a Tax Refund submission confirmation",
+      plainTemplate = txt.dfsSubmissionConfirmationEmail_welsh.f,
+      htmlTemplate = html.dfsSubmissionConfirmationEmail_welsh.f,
+      priority = Some(MessagePriority.Urgent)
+    ),
+    MessageTemplate.create(
       templateId = "dfs_submission_success_cis_2015",
       fromAddress = govUkTeamAddress,
       service = DigitalFormsService,
       subject = "Subcontractor registration submission confirmation",
       plainTemplate = txt.dfsSubmissionConfirmationEmailCIS.f,
       htmlTemplate = html.dfsSubmissionConfirmationEmailCIS.f,
+      priority = Some(MessagePriority.Urgent)
+    ),
+    MessageTemplate.create(
+      templateId = "dfs_submission_success_cis_2015_welsh",
+      fromAddress = govUkTeamAddress,
+      service = DigitalFormsService,
+      subject = "Subcontractor registration submission confirmation",
+      plainTemplate = txt.dfsSubmissionConfirmationEmailCIS_welsh.f,
+      htmlTemplate = html.dfsSubmissionConfirmationEmailCIS_welsh.f,
       priority = Some(MessagePriority.Urgent)
     ),
     MessageTemplate.create(
@@ -50,6 +68,15 @@ object DfsTemplates {
       htmlTemplate = html.dfsSubmissionConfirmationEmailSPTU.f,
       priority = Some(MessagePriority.Urgent)
     ),
+    MessageTemplate.create(
+      templateId = "dfs_submission_success_sptu_2015_welsh",
+      fromAddress = govUkTeamAddress,
+      service = DigitalFormsService,
+      subject = "State Pension top up (Class3A) submission confirmation",
+      plainTemplate = txt.dfsSubmissionConfirmationEmailSPTU_welsh.f,
+      htmlTemplate = html.dfsSubmissionConfirmationEmailSPTU_welsh.f,
+      priority = Some(MessagePriority.Urgent)
+    ),
     MessageTemplate.createWithDynamicSubject(
       templateId = "dfs_submission_success_generic_2015",
       fromAddress = govUkTeamAddress,
@@ -58,13 +85,32 @@ object DfsTemplates {
       plainTemplate = txt.dfsSubmissionConfirmationEmailGeneric.f,
       htmlTemplate = html.dfsSubmissionConfirmationEmailGeneric.f,
       priority = Some(MessagePriority.Urgent)
-    ), MessageTemplate.createWithDynamicSubject(
+    ),
+    MessageTemplate.createWithDynamicSubject(
+      templateId = "dfs_submission_success_generic_2015_welsh",
+      fromAddress = govUkTeamAddress,
+      service = DigitalFormsService,
+      subject = _.apply("subject"),
+      plainTemplate = txt.dfsSubmissionConfirmationEmailGeneric_welsh.f,
+      htmlTemplate = html.dfsSubmissionConfirmationEmailGeneric_welsh.f,
+      priority = Some(MessagePriority.Urgent)
+    ),
+    MessageTemplate.createWithDynamicSubject(
       templateId = "dfs_submission_success_generic_2017",
       fromAddress = govUkTeamAddress,
       service = DigitalFormsService,
       subject = _.apply("subject"),
       plainTemplate = txt.dfsSubmissionConfirmationEmailGeneric3Paragraphs.f,
       htmlTemplate = html.dfsSubmissionConfirmationEmailGeneric3Paragraphs.f,
+      priority = Some(MessagePriority.Urgent)
+    ),
+    MessageTemplate.createWithDynamicSubject(
+      templateId = "dfs_submission_success_generic_2017_welsh",
+      fromAddress = govUkTeamAddress,
+      service = DigitalFormsService,
+      subject = _.apply("subject"),
+      plainTemplate = txt.dfsSubmissionConfirmationEmailGeneric3Paragraphs_welsh.f,
+      htmlTemplate = html.dfsSubmissionConfirmationEmailGeneric3Paragraphs_welsh.f,
       priority = Some(MessagePriority.Urgent)
     ),
     MessageTemplate.createWithDynamicSubject(
@@ -76,6 +122,15 @@ object DfsTemplates {
       htmlTemplate = html.dfsSubmissionConfirmationEmailEmptyTurnAroundTime.f,
       priority = Some(MessagePriority.Urgent)
     ),
+    MessageTemplate.createWithDynamicSubject(
+      templateId = "dfs_submission_success_empty_turn_around_time_2015_welsh",
+      fromAddress = govUkTeamAddress,
+      service = DigitalFormsService,
+      subject = _.apply("subject"),
+      plainTemplate = txt.dfsSubmissionConfirmationEmailEmptyTurnAroundTime_welsh.f,
+      htmlTemplate = html.dfsSubmissionConfirmationEmailEmptyTurnAroundTime_welsh.f,
+      priority = Some(MessagePriority.Urgent)
+    ),
     MessageTemplate.create(
       templateId = "dfs_admin_notification",
       fromAddress = govUkTeamAddress,
@@ -83,6 +138,15 @@ object DfsTemplates {
       subject = "You’ve got a new message from HMRC Child Benefit admin",
       plainTemplate = txt.dfsAdminNotificationEmail.f,
       htmlTemplate = html.dfsAdminNotificationEmail.f,
+      priority = Some(MessagePriority.Urgent)
+    ),
+    MessageTemplate.create(
+      templateId = "dfs_admin_notification_welsh",
+      fromAddress = govUkTeamAddress,
+      service = DigitalFormsService,
+      subject = "You’ve got a new message from HMRC Child Benefit admin",
+      plainTemplate = txt.dfsAdminNotificationEmail_welsh.f,
+      htmlTemplate = html.dfsAdminNotificationEmail_welsh.f,
       priority = Some(MessagePriority.Urgent)
     ),
     MessageTemplate.createWithDynamicSubject(
@@ -94,6 +158,15 @@ object DfsTemplates {
       htmlTemplate = html.dfsTrustsSubmissionConfirmationEmail.f,
       priority = Some(MessagePriority.Urgent)
     ),
+    MessageTemplate.createWithDynamicSubject(
+      templateId = "dfs_trusts_submission_success_welsh",
+      fromAddress = govUkTeamAddress,
+      service = DigitalFormsService,
+      subject = _.apply("subject"),
+      plainTemplate = txt.dfsTrustsSubmissionConfirmationEmail_welsh.f,
+      htmlTemplate = html.dfsTrustsSubmissionConfirmationEmail_welsh.f,
+      priority = Some(MessagePriority.Urgent)
+    ),
     MessageTemplate.create(
       templateId = "dfs_submission_success_rcgt_2018",
       fromAddress = govUkTeamAddress,
@@ -101,6 +174,15 @@ object DfsTemplates {
       subject = "Capital Gains Tax real time transaction return submission confirmation",
       plainTemplate = txt.dfsSubmissionConfirmationEmailRCGT.f,
       htmlTemplate = html.dfsSubmissionConfirmationEmailRCGT.f,
+      priority = Some(MessagePriority.Urgent)
+    ),
+    MessageTemplate.create(
+      templateId = "dfs_submission_success_rcgt_2018_welsh",
+      fromAddress = govUkTeamAddress,
+      service = DigitalFormsService,
+      subject = "Capital Gains Tax real time transaction return submission confirmation",
+      plainTemplate = txt.dfsSubmissionConfirmationEmailRCGT_welsh.f,
+      htmlTemplate = html.dfsSubmissionConfirmationEmailRCGT_welsh.f,
       priority = Some(MessagePriority.Urgent)
     )
   )
