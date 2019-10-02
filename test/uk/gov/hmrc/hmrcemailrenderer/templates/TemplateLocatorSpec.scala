3d0feec5fec8bc514bfe09b76b1137774bf06fee
Jonathan-Courage <48124229+Jonathan-Courage@users.noreply.github.com>
2019-10-02 08:59:33 +0100

    Adding CCG1 email template (#391)

--- test/uk/gov/hmrc/hmrcemailrenderer/templates/TemplateLocatorSpec.scala
+++ test/uk/gov/hmrc/hmrcemailrenderer/templates/TemplateLocatorSpec.scala
@@ -232,6 +232,7 @@ class TemplateLocatorSpec extends UnitSpec with OneAppPerSuite {
         "cet_email_confirmation",
         "gss_email_confirmation",
         "cash_declaration",
+        "ccg1_confirmation_submission",
         "register_for_duty_on_gas_for_road_fuel_use",
         "register_for_duty_on_biofuels_and_other_fuel_substitutes",
         "register_for_fuel_duty_confirmation",
