3d5692b3f62a24985d15e89ee0907c0dc29cbcc9
Alex Olkhovskiy <lxol@users.noreply.github.com>
2020-01-27 16:40:41 +0000

    [DC-2113] Update findTemplate to fallback to English template

--- app/uk/gov/hmrc/hmrcemailrenderer/templates/TemplateLocator.scala
+++ app/uk/gov/hmrc/hmrcemailrenderer/templates/TemplateLocator.scala
@@ -118,6 +118,15 @@ trait TemplateLocator {
   lazy val all: Seq[MessageTemplate] = templateGroups.values.flatten.toSeq
 
   def findTemplate(templateId: String): Option[MessageTemplate] =
-    all.find(_.templateId == templateId)
+    all.find(_.templateId == templateId) orElse  {
+      if (templateId.endsWith(TemplateLocator.WELSH_SUFFIX)) {
+        all.find(_.templateId == templateId.stripSuffix(TemplateLocator.WELSH_SUFFIX))
+      } else {
+        None
+      }
+    }
 }
-object TemplateLocator extends TemplateLocator
+
+object TemplateLocator extends TemplateLocator {
+  final val WELSH_SUFFIX = ".cy"
+}
