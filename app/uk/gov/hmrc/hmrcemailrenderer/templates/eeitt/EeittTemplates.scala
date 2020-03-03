017927bea6468ddd90812a74605bb62bc002999d
Jonathan-Courage <jcourage@live.co.uk>
2020-03-03 10:16:01 +0000

    Confirmation code updates

--- app/uk/gov/hmrc/hmrcemailrenderer/templates/eeitt/EeittTemplates.scala
+++ app/uk/gov/hmrc/hmrcemailrenderer/templates/eeitt/EeittTemplates.scala
@@ -536,11 +536,11 @@ object EeittTemplates {
       htmlTemplate = html.civTransparencyConfirmation.f,
       priority = Some(MessagePriority.Standard)
     ),
-    MessageTemplate.create(
+    MessageTemplate.createWithDynamicSubject(
       templateId = "confirmationCode_confirmation_submission",
       fromAddress = govUkTeamAddress,
       service = Eeitt,
-      subject = "Confirm your email address",
+      subject = extractSubject,
       plainTemplate = txt.confirmationCodeTemplate.f,
       htmlTemplate = html.confirmationCodeTemplate.f,
       priority = Some(MessagePriority.Standard)
@@ -573,4 +573,8 @@ object EeittTemplates {
       priority = Some(MessagePriority.Standard)
     )
   )
+  private def extractSubject(params: Map[String, String]) =
+    params.get("emailSubject")
+      .map(subTitle => s"Confirm your email address '$subTitle'")
+      .getOrElse(throw new RuntimeException("Missing parameter emailSubject"))
 }
