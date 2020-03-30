7a16855fca748570b9968c3ad326517bf20831ae
Alistair Macdonald <1460971+nubz@users.noreply.github.com>
2020-03-30 12:31:52 +0100

    cpdp-1257: removed variables and changed content as per ticket updates

--- app/preview/TemplateParams.scala
+++ app/preview/TemplateParams.scala
@@ -1640,10 +1640,7 @@ object TemplateParams {
     ),
     "cgtpd_submission_confirmation" -> Map(
       "name" -> "Jamie Wilson",
-      "submissionId" -> "000000000006",
-      "taxDue" -> "£2,650.53",
-      "chargeRef" -> "XCRG3333333336",
-      "dueDate" -> "5 October 2020"
+      "submissionId" -> "000000000006"
     ),
     "dst_registration_accepted" -> Map(
         "dstNumber" -> "XADST0000010000",
