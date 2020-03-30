a76eef3a1c188fe91d3217f7dd807fa3f629ec52
Alistair Macdonald <1460971+nubz@users.noreply.github.com>
2020-03-30 16:22:04 +0100

    cpdp-1257: missing word 'has'

--- app/uk/gov/hmrc/hmrcemailrenderer/templates/cgtpd/CgtpdTemplates.scala
+++ app/uk/gov/hmrc/hmrcemailrenderer/templates/cgtpd/CgtpdTemplates.scala
@@ -45,7 +45,7 @@ object CgtpdTemplates {
       templateId ="cgtpd_submission_confirmation",
       fromAddress = govUkTeamAddress,
       service = Cgtpd,
-      subject = "HMRC received your return",
+      subject = "HMRC has received your return",
       plainTemplate = txt.cgtpdSubmissionConfirmation.f,
       htmlTemplate = html.cgtpdSubmissionConfirmation.f,
       priority = Some(MessagePriority.Urgent)
