1f3c0a10834ab097d17fc2c24e4f2209daf00dd7
Daniel Connelly <daniel.connelly@hotmail.co.uk>
2020-05-14 16:47:32 +0100

    RORO-328 adding email template for get a goods movement service that indicates the user has registred to use our service

---
+++ app/uk/gov/hmrc/hmrcemailrenderer/templates/gms/GmsTemplates.scala
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.hmrcemailrenderer.templates.gms
+
+import uk.gov.hmrc.hmrcemailrenderer.domain.MessageTemplate
+import uk.gov.hmrc.hmrcemailrenderer.templates.FromAddress
+import uk.gov.hmrc.hmrcemailrenderer.templates.ServiceIdentifier.GoodsMovementSystem
+
+object GmsTemplates {
+
+  val templates = Seq(
+    MessageTemplate.create(
+      templateId = "gms_enrolment_confirmation",
+      fromAddress = FromAddress.noReply("Get a goods movement reference"),
+      service = GoodsMovementSystem,
+      subject = "Goods movement references registration",
+      plainTemplate = txt.gms_enrolment_confirmation.f,
+      htmlTemplate = html.gms_enrolment_confirmation.f,
+      priority = None
+    )
+  )
+}
