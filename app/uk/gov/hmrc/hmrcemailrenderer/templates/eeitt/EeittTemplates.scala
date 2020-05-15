37c6dd60b9f2a744d741d0db652ef703f4eea8ad
Elie Labeca <1290092+elabeca@users.noreply.github.com>
2020-05-15 09:58:22 +0100

    DC-2490 - scalafmt

--- app/uk/gov/hmrc/hmrcemailrenderer/templates/eeitt/EeittTemplates.scala
+++ app/uk/gov/hmrc/hmrcemailrenderer/templates/eeitt/EeittTemplates.scala
@@ -16,7 +16,7 @@
 
 package uk.gov.hmrc.hmrcemailrenderer.templates.eeitt
 
-import uk.gov.hmrc.hmrcemailrenderer.templates.FromAddress.{govUkTeamAddress, seissFromAddress, seissFromAddressWelsh}
+import uk.gov.hmrc.hmrcemailrenderer.templates.FromAddress.{ govUkTeamAddress, seissFromAddress, seissFromAddressWelsh }
 import uk.gov.hmrc.hmrcemailrenderer.domain.{ MessagePriority, MessageTemplate }
 import uk.gov.hmrc.hmrcemailrenderer.templates.ServiceIdentifier.Eeitt
 
