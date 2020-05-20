e758b1983fb4ade557e5caf753d4c01ea0c242df
shridhaarchippaa <15193271+shridhaarchippaa@users.noreply.github.com>
2020-05-20 19:02:31 +0100

    added test case for gms

--- app/preview/TemplateParams.scala
+++ app/preview/TemplateParams.scala
@@ -1820,6 +1820,9 @@ object TemplateParams {
       "hmrcEmail"        -> "pensions.administration@hmrc.gov.uk",
       "psaName"          -> "Jane Doe",
       "submissionNumber" -> "3"
+    ),
+    "gms_enrolment_confirmation" -> Map(
+      "userName"-> "Customer"
     )
   )
 }
