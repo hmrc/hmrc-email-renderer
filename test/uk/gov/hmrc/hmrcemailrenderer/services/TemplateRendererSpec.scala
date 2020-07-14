54190292d31f8d9f275308f98940ef722fb42c6a
Elie Labeca <1290092+elabeca@users.noreply.github.com>
2020-07-14 13:22:28 +0100

    DC-2615 - Adding splunk events and kibana logging for lang pref template selection

--- test/uk/gov/hmrc/hmrcemailrenderer/services/TemplateRendererSpec.scala
+++ test/uk/gov/hmrc/hmrcemailrenderer/services/TemplateRendererSpec.scala
@@ -16,21 +16,25 @@
 
 package uk.gov.hmrc.hmrcemailrenderer.services
 
+import org.mockito.ArgumentCaptor
 import org.mockito.Matchers._
 import org.mockito.Mockito._
 import org.scalatest.concurrent.Eventually
 import org.scalatest.mockito.MockitoSugar
 import uk.gov.hmrc.hmrcemailrenderer.connectors.PreferencesConnector
 import uk.gov.hmrc.hmrcemailrenderer.controllers.model.RenderResult
-import uk.gov.hmrc.hmrcemailrenderer.domain.{ MessagePriority, MessageTemplate, MissingTemplateId, TemplateRenderFailure }
+import uk.gov.hmrc.hmrcemailrenderer.domain.{MessagePriority, MessageTemplate, MissingTemplateId, TemplateRenderFailure}
 import uk.gov.hmrc.hmrcemailrenderer.model.Language
 import uk.gov.hmrc.hmrcemailrenderer.templates.ServiceIdentifier.SelfAssessment
 import uk.gov.hmrc.hmrcemailrenderer.templates.TemplateLocator
 import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.play.audit.http.connector.{AuditConnector, AuditResult}
+import uk.gov.hmrc.play.audit.model.DataEvent
+import uk.gov.hmrc.play.audit.model.EventTypes.Succeeded
 import uk.gov.hmrc.play.test.UnitSpec
 
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 
 class TemplateRendererSpec extends UnitSpec with MockitoSugar {
 
@@ -57,41 +61,146 @@ class TemplateRendererSpec extends UnitSpec with MockitoSugar {
 
   "LanguageTemplateId" should {
 
-    "return the same template if the template doesnt exist in WelshTemplatesByLangPreference object and email preference is English" in new TestCase {
+    "return the same template if the template doesn't exist in WelshTemplatesByLangPreference object and email preference is English" in new TestCase {
+      val dataEventArgumentCaptor = ArgumentCaptor.forClass(classOf[DataEvent])
+
+      when(templateRenderer.auditConnector.sendEvent(any[DataEvent])(any[HeaderCarrier], any[ExecutionContext]))
+        .thenReturn(Future.successful(AuditResult.Success))
       when(locatorMock.findTemplate(templateId)).thenReturn(Some(validTemplate))
-      when(templateRenderer.preferencesConnector.languageByEmail(anyString())(any()))
+      when(templateRenderer.preferencesConnector.languageByEmail(anyString())(any[HeaderCarrier]))
         .thenReturn(Future.successful(Language.English))
+
       await(templateRenderer.languageTemplateId(templateId, Some("test@test.com"))) shouldBe templateId
+
+      verify(templateRenderer.auditConnector).sendEvent(dataEventArgumentCaptor.capture())(any[HeaderCarrier], any[ExecutionContext])
+
+      dataEventArgumentCaptor.getValue.auditSource shouldBe "hmrc-email-renderer"
+      dataEventArgumentCaptor.getValue.auditType shouldBe "TxSucceeded"
+      dataEventArgumentCaptor.getValue.detail shouldBe Map(
+        "email" -> "test@test.com",
+        "description" -> "Defaulting to English",
+        "originalTemplateId" -> templateId,
+        "selectedTemplateId" -> templateId,
+        "language" -> "English",
+        "engTemplateId" -> "welshTemplateId")
     }
 
     "return welsh template if template is in WelshTemplatesByLangPreference and language preferences set to welsh" in new TestCase {
-      when(templateRenderer.preferencesConnector.languageByEmail(anyString())(any()))
+      val dataEventArgumentCaptor = ArgumentCaptor.forClass(classOf[DataEvent])
+
+      when(templateRenderer.auditConnector.sendEvent(any[DataEvent])(any[HeaderCarrier], any[ExecutionContext]))
+        .thenReturn(Future.successful(AuditResult.Success))
+      when(templateRenderer.preferencesConnector.languageByEmail(anyString())(any[HeaderCarrier]))
         .thenReturn(Future.successful(Language.Welsh))
+
       await(templateRenderer.languageTemplateId(engTemplateId, Some("test@test.com"))) shouldBe welshTemplateId
+
+      verify(templateRenderer.auditConnector).sendEvent(dataEventArgumentCaptor.capture())(any[HeaderCarrier], any[ExecutionContext])
+
+      dataEventArgumentCaptor.getValue.auditSource shouldBe "hmrc-email-renderer"
+      dataEventArgumentCaptor.getValue.auditType shouldBe "TxSucceeded"
+      dataEventArgumentCaptor.getValue.detail shouldBe Map(
+        "email" -> "test@test.com",
+        "description" -> "Language preference found",
+        "originalTemplateId" -> engTemplateId,
+        "selectedTemplateId" -> welshTemplateId,
+        "language" -> "Welsh",
+        "engTemplateId" -> "welshTemplateId")
     }
 
     "return english template if template is in WelshTemplatesByLangPreference and language preferences set to english" in new TestCase {
-      when(templateRenderer.preferencesConnector.languageByEmail(anyString())(any()))
+      val dataEventArgumentCaptor = ArgumentCaptor.forClass(classOf[DataEvent])
+
+      when(templateRenderer.auditConnector.sendEvent(any[DataEvent])(any[HeaderCarrier], any[ExecutionContext]))
+        .thenReturn(Future.successful(AuditResult.Success))
+      when(templateRenderer.preferencesConnector.languageByEmail(anyString())(any[HeaderCarrier]))
         .thenReturn(Future.successful(Language.English))
+
       await(templateRenderer.languageTemplateId(engTemplateId, Some("test@test.com"))) shouldBe engTemplateId
+
+      verify(templateRenderer.auditConnector).sendEvent(dataEventArgumentCaptor.capture())(any[HeaderCarrier], any[ExecutionContext])
+
+      dataEventArgumentCaptor.getValue.auditSource shouldBe "hmrc-email-renderer"
+      dataEventArgumentCaptor.getValue.auditType shouldBe "TxSucceeded"
+      dataEventArgumentCaptor.getValue.detail shouldBe Map(
+        "email" -> "test@test.com",
+        "description" -> "Language preference found",
+        "originalTemplateId" -> engTemplateId,
+        "selectedTemplateId" -> engTemplateId,
+        "language" -> "English",
+        "engTemplateId" -> "welshTemplateId")
     }
 
-    "return same template if the template doesnt exist in WelshTemplatesByLangPreference object and language preference is Welsh" in new TestCase {
-      when(templateRenderer.preferencesConnector.languageByEmail(anyString())(any()))
+    "return same template if the template doesn't exist in WelshTemplatesByLangPreference object and language preference is Welsh" in new TestCase {
+      val dataEventArgumentCaptor = ArgumentCaptor.forClass(classOf[DataEvent])
+
+      when(templateRenderer.auditConnector.sendEvent(any[DataEvent])(any[HeaderCarrier], any[ExecutionContext]))
+        .thenReturn(Future.successful(AuditResult.Success))
+      when(templateRenderer.preferencesConnector.languageByEmail(anyString())(any[HeaderCarrier]))
         .thenReturn(Future.successful(Language.Welsh))
+
       await(templateRenderer.languageTemplateId(templateId, Some("test@test.com"))) shouldBe templateId
+
+      verify(templateRenderer.auditConnector).sendEvent(dataEventArgumentCaptor.capture())(any[HeaderCarrier], any[ExecutionContext])
+
+      dataEventArgumentCaptor.getValue.auditSource shouldBe "hmrc-email-renderer"
+      dataEventArgumentCaptor.getValue.auditType shouldBe "TxSucceeded"
+      dataEventArgumentCaptor.getValue.detail shouldBe Map(
+        "email" -> "test@test.com",
+        "description" -> "Defaulting to English",
+        "originalTemplateId" -> templateId,
+        "selectedTemplateId" -> templateId,
+        "language" -> "English",
+        "engTemplateId" -> "welshTemplateId"
+      )
     }
 
-    "return same template if the template doesnt exist in WelshTemplatesByLangPreference object and no email is provided" in new TestCase {
+    "return same template if the template doesn't exist in WelshTemplatesByLangPreference object and no email is provided" in new TestCase {
+      val dataEventArgumentCaptor = ArgumentCaptor.forClass(classOf[DataEvent])
+
+      when(templateRenderer.auditConnector.sendEvent(any[DataEvent])(any[HeaderCarrier], any[ExecutionContext]))
+        .thenReturn(Future.successful(AuditResult.Success))
+
       await(templateRenderer.languageTemplateId(templateId, None)) shouldBe templateId
+
+      verify(templateRenderer.auditConnector).sendEvent(dataEventArgumentCaptor.capture())(any[HeaderCarrier], any[ExecutionContext])
+
+      dataEventArgumentCaptor.getValue.auditSource shouldBe "hmrc-email-renderer"
+      dataEventArgumentCaptor.getValue.auditType shouldBe "TxSucceeded"
+      dataEventArgumentCaptor.getValue.detail shouldBe Map(
+        "email" -> "N/A",
+        "description" -> "Defaulting to English",
+        "originalTemplateId" -> templateId,
+        "selectedTemplateId" -> templateId,
+        "language" -> "English",
+        "engTemplateId" -> "welshTemplateId"
+      )
     }
 
     "return same template if the template exist in WelshTemplatesByLangPreference object and no email is provided" in new TestCase {
+      val dataEventArgumentCaptor = ArgumentCaptor.forClass(classOf[DataEvent])
+
+      when(templateRenderer.auditConnector.sendEvent(any[DataEvent])(any[HeaderCarrier], any[ExecutionContext]))
+        .thenReturn(Future.successful(AuditResult.Success))
+
       await(templateRenderer.languageTemplateId(engTemplateId, None)) shouldBe engTemplateId
+
+      verify(templateRenderer.auditConnector).sendEvent(dataEventArgumentCaptor.capture())(any[HeaderCarrier], any[ExecutionContext])
+
+      dataEventArgumentCaptor.getValue.auditSource shouldBe "hmrc-email-renderer"
+      dataEventArgumentCaptor.getValue.auditType shouldBe "TxSucceeded"
+      dataEventArgumentCaptor.getValue.detail shouldBe Map(
+        "email" -> "N/A",
+        "description" -> "Defaulting to English",
+        "originalTemplateId" -> engTemplateId,
+        "selectedTemplateId" -> engTemplateId,
+        "language" -> "English",
+        "engTemplateId" -> "welshTemplateId")
     }
   }
 
   class TestCase {
+
     val locatorMock = mock[TemplateLocator]
     val templateId = "a-template-id"
     val engTemplateId = "engTemplateId"
@@ -107,6 +216,7 @@ class TemplateRendererSpec extends UnitSpec with MockitoSugar {
 
       override val preferencesConnector = mock[PreferencesConnector]
 
+      override val auditConnector = mock[AuditConnector]
     }
 
     val validTemplate = MessageTemplate.create(
