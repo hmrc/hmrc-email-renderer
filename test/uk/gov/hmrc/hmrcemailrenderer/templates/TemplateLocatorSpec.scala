9a21054bc940e635510f193195327c6f3c98c667
George Herbert <georgeherby@hotmail.com>
2020-08-11 10:19:32 +0100

    [SEISS-578][DC-2695] Add seissPhase2PreviouslyClaimedStagger template (english) (#552)
    * Add seissPhase2PreviouslyClaimedStagger template (english)
    
    * Add welsh template for seissPhase2PreviouslyClaimedStagger
    
    Co-authored-by: Alex Olkhovskiy <lxol@users.noreply.github.com>

--- test/uk/gov/hmrc/hmrcemailrenderer/templates/TemplateLocatorSpec.scala
+++ test/uk/gov/hmrc/hmrcemailrenderer/templates/TemplateLocatorSpec.scala
@@ -296,6 +296,8 @@ class TemplateLocatorSpec extends UnitSpec with OneAppPerSuite {
         "seiss_payment_failed",
         "seiss_payment_failed_cy",
         "seiss_received_confirmation",
+        "seiss_phase_2_previously_claimed_stagger",
+        "seiss_phase_2_previously_claimed_stagger_cy",
         "cjrs_confirmation_submission",
         "csr_submission_confirmation",
         "cet_email_confirmation",
