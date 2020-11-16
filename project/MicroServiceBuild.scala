13b15a2696f94638afec585da5cb020ca8e21987
muralishub <muralispost@gmail.com>
2020-11-16 10:26:24 +0000

    Dc 2086 (#614)
    * DC-2086: production code without proper html
    
    * DC-2086: complete work on  "default-src 'self' 'unsafe-inline'"
    
    * DC-2086: complete work on  "default-src 'self' 'unsafe-inline'"
    
    * DC-2086: unsafe inline
    
    * DC-2086: Play 2.6 upgrade
    
    * DC-2086: Play 2.6 upgrade

--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -12,17 +12,17 @@ object MicroServiceBuild extends Build with MicroService {
 private object AppDependencies {
   def apply() = Seq(
     ws,
-    "uk.gov.hmrc"                 %% "microservice-bootstrap" % "10.9.0",
-    "uk.gov.hmrc"                 %% "domain"                 % "5.6.0-play-25",
-    "uk.gov.hmrc"                 %% "emailaddress"           % "3.3.0",
-    "net.codingwell"              %% "scala-guice"            % "4.1.1",
-    "com.beachape"                %% "enumeratum"             % "1.5.13",
-    "org.jsoup"                   %  "jsoup"                  % "1.7.3"             % "test",
-    "org.scalatestplus.play"      %% "scalatestplus-play"     % "2.0.1"             % "test, it",
-    "uk.gov.hmrc"                 %% "hmrctest"               % "3.9.0-play-25"     % "test, it",
+    "uk.gov.hmrc"                 %% "bootstrap-play-26" % "1.16.0",
+    "uk.gov.hmrc"                 %% "domain"                 % "5.9.0-play-26",
+    "uk.gov.hmrc"                 %% "emailaddress"           % "3.5.0",
+    "net.codingwell"              %% "scala-guice"            % "4.2.6",
+    "com.beachape"                %% "enumeratum"             % "1.6.1",
+    "org.jsoup"                   %  "jsoup"                  % "1.13.1"             % "test",
+    "org.scalatestplus.play"      %% "scalatestplus-play"     % "3.1.3"             % "test, it",
+    "uk.gov.hmrc"                 %% "hmrctest"               % "3.9.0-play-26"     % "test, it",
     "org.pegdown"                 %  "pegdown"                % "1.6.0"             % "test, it",
-    "org.mockito"                 %  "mockito-core"           % "1.9.0"             % "test, it",
-    "com.github.tomakehurst"      % "wiremock"                % "1.58"              % "test,it"
+    "org.mockito"                 %  "mockito-core"           % "3.6.0"             % "test, it",
+    "com.github.tomakehurst"      % "wiremock"                % "2.27.2"              % "test,it"
   )
 }
 
