2ab193a735b113e7538c269ef33d18e0f7f0d05d
stephen-oppong-beduh <56881842+stephen-oppong-beduh@users.noreply.github.com>
2020-11-30 14:40:27 +0000

    DL-4451 - Added new empty_turn_around_time template (#594)

--- test/uk/gov/hmrc/hmrcemailrenderer/templates/TemplateLocatorSpec.scala
+++ test/uk/gov/hmrc/hmrcemailrenderer/templates/TemplateLocatorSpec.scala
@@ -229,6 +229,7 @@ class TemplateLocatorSpec extends UnitSpec with OneAppPerSuite {
         "dfs_submission_success_generic_2017_welsh",
         "dfs_submission_success_empty_turn_around_time_2015",
         "dfs_submission_success_empty_turn_around_time_2015_welsh",
+        "dfs_submission_success_empty_turn_around_time_2020",
         "dfs_admin_notification",
         "dfs_admin_notification_welsh",
         "dfs_trusts_submission_success",
