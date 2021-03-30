652d7d830edb6ab28f36464a26225c63de65ff45
satyajosyula2020 <satya.josyula@digital.hmrc.gov.uk>
2021-03-30 17:22:04 +0100

    Revert "[MP][OPS-6008] CDS DDI Instruction not acceptable (DD02) (#711)"
    This reverts commit db8615c4c46c5ac69bfca468eb10184ccf83adff.

--- test/uk/gov/hmrc/hmrcemailrenderer/templates/TemplateLocatorSpec.scala
+++ test/uk/gov/hmrc/hmrcemailrenderer/templates/TemplateLocatorSpec.scala
@@ -568,7 +568,6 @@ class TemplateLocatorSpec extends UnitSpec with OneAppPerSuite {
         "sdds_ddi_cancelled_dcs_alert",
         "sdds_ddi_reminder_dcs_alert",
         "sdds_ddi_unpaid_dcs_alert",
-        "cds_ddi_unpaid_dcs_alert",
         "hts_verification_email",
         "sdil_registration_accepted",
         "sdil_registration_received",
