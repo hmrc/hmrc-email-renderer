248b8d0e4079799370b8523c9926cda3c4c897b4
susan-badel <57998768+susan-badel@users.noreply.github.com>
2021-07-15 13:09:23 +0100

    Updated content for OSS registrations confirmation email VEOSS-228 (#783)
    * Updated content for OSS registrations confirmation email VEOSS-228
    
    * Ran scalafmt
    
    * Ran test:scalafmt
    
    * Fixed oss issues

--- app/preview/TemplateParams.scala
+++ app/preview/TemplateParams.scala
@@ -1563,10 +1563,12 @@ object TemplateParams {
       "deregisterDate" -> "6 June 2018"
     ),
     "oss_registration_confirmation" -> Map(
-      "recipientName_line1" -> "Joe Bloggs",
-      "businessName"        -> "Test Business",
-      "startDate"           -> "12 June 2021",
-      "reference"           -> "123456789"
+      "recipientName_line1"                -> "Joe Bloggs",
+      "businessName"                       -> "Test Business",
+      "startDate"                          -> "12 June 2021",
+      "reference"                          -> "123456789",
+      "lastDayOfCalendarQuarter"           -> "30 September 2021",
+      "lastDayOfMonthAfterCalendarQuarter" -> "31 October 2021"
     )
   )
 }
@@ -2601,10 +2603,12 @@ object TemplateParams2 {
     "newMessageAlert_PA302_D2"    -> newMessageAlert_Names,
     "newMessageAlert_PA302_D2_cy" -> newMessageAlert_Names,
     "oss_registration_confirmation" -> Map(
-      "recipientName_line1" -> "Joe Bloggs",
-      "businessName"        -> "Test Business",
-      "startDate"           -> "12 June 2021",
-      "reference"           -> "123456789"
+      "recipientName_line1"                -> "Joe Bloggs",
+      "businessName"                       -> "Test Business",
+      "startDate"                          -> "12 June 2021",
+      "reference"                          -> "123456789",
+      "lastDayOfCalendarQuarter"           -> "30 September 2021",
+      "lastDayOfMonthAfterCalendarQuarter" -> "31 October 2021"
     )
   )
 }
