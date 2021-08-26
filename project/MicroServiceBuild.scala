d249c42a3139533df6e418d6441f634d6a90905d
Kapil Sanghvi <22366606+kapil-sanghvi@users.noreply.github.com>
2021-08-26 13:22:29 +0100

    Update dependencies to resolve the abstract method error (#791)

--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -12,17 +12,16 @@ object MicroServiceBuild extends Build with MicroService {
 private object AppDependencies {
   def apply() = Seq(
     ws,
-    "uk.gov.hmrc"            %% "bootstrap-frontend-play-27"  % "4.0.0",
-    "uk.gov.hmrc"            %% "auth-client"        % "5.6.0-play-27",
-    "uk.gov.hmrc"            %% "domain"             % "6.2.0-play-27",
-    "uk.gov.hmrc"            %% "emailaddress"       % "3.5.0",
-    "net.codingwell"         %% "scala-guice"        % "4.2.6",
-    "com.beachape"           %% "enumeratum"         % "1.6.1",
-    "org.jsoup"              % "jsoup"               % "1.13.1" % "test",
-    "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.3" % "test, it",
-    "uk.gov.hmrc"            %% "service-integration-test" % "0.13.0-play-27" % "test, it",
-    "org.pegdown"            % "pegdown"             % "1.6.0" % "test, it",
-    "org.mockito"            % "mockito-core"        % "3.6.0" % "test, it",
-    "com.github.tomakehurst" % "wiremock"            % "2.27.2" % "test,it"
+    "uk.gov.hmrc"            %% "bootstrap-frontend-play-27" % "5.10.0",
+    "uk.gov.hmrc"            %% "domain"                     % "6.2.0-play-27",
+    "uk.gov.hmrc"            %% "emailaddress"               % "3.5.0",
+    "net.codingwell"         %% "scala-guice"                % "4.2.6",
+    "com.beachape"           %% "enumeratum"                 % "1.6.1",
+    "org.jsoup"              % "jsoup"                       % "1.13.1" % "test",
+    "org.scalatestplus.play" %% "scalatestplus-play"         % "3.1.3" % "test, it",
+    "uk.gov.hmrc"            %% "service-integration-test"   % "0.13.0-play-27" % "test, it",
+    "org.pegdown"            % "pegdown"                     % "1.6.0" % "test, it",
+    "org.mockito"            % "mockito-core"                % "3.6.0" % "test, it",
+    "com.github.tomakehurst" % "wiremock"                    % "2.27.2" % "test,it"
   )
 }
