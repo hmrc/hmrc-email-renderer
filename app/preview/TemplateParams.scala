9aa39ebb7dbed7e96cac370fc72779944e982bde
David Ansah <45106722+ansahdavid@users.noreply.github.com>
2021-09-28 15:48:33 +0100

    Have added OSS Returns Email Confirmation No VAT Owed Template (#801)

--- app/preview/TemplateParams.scala
+++ app/preview/TemplateParams.scala
@@ -1587,6 +1587,11 @@ object TemplateParams {
       "paymentDeadline"     -> "31 October 2021",
       "VATOwed"             -> "£1250",
       "reference"           -> "XI/XI100000002/Q3.2021"
+    ),
+    "oss_returns_email_confirmation_no_vat_owed" -> Map(
+      "recipientName_line1" -> "Joe Bloggs",
+      "period"              -> "1 July to 30 September 2021",
+      "reference"           -> "XI/XI100000002/Q3.2021"
     )
   )
 }
@@ -2717,6 +2722,11 @@ object TemplateParams2 {
       "period"              -> "1 July to 30 September 2021",
       "paymentDeadline"     -> "31 October 2021",
       "VATOwed"             -> "£1250",
+      "reference"           -> "XI/XI100000002/Q3.2021"
+    ),
+    "oss_returns_email_confirmation_no_vat_owed" -> Map(
+      "recipientName_line1" -> "Joe Bloggs",
+      "period"              -> "1 July to 30 September 2021",
       "reference"           -> "XI/XI100000002/Q3.2021"
     ),
     "nipbe_confirmation" -> Map(
