838d9164c850b46f4effd4382f20274cd00872ae
Elie Labeca <1290092+elabeca@users.noreply.github.com>
2021-12-06 09:54:55 +0000

    DC-3852 - scoverage settings (#828)

--- project/ScoverageSettings.scala
+++ project/ScoverageSettings.scala
@@ -22,8 +22,8 @@ object ScoverageSettings {
   def apply(): Seq[Def.Setting[_ >: String with Double with Boolean]] =
     Seq( // Semicolon-separated list of regexes matching classes to exclude
       ScoverageKeys.coverageExcludedPackages := "<empty>;.*Reverse.*;.*(config|testonly).*;.*(BuildInfo|Routes).*",
-      ScoverageKeys.coverageMinimum := 95.00,
-      ScoverageKeys.coverageFailOnMinimum := false,
+      ScoverageKeys.coverageMinimum := 94.02,
+      ScoverageKeys.coverageFailOnMinimum := true,
       ScoverageKeys.coverageHighlighting := true,
       parallelExecution in ConfigKey.configurationToKey(Test) := false
     )
