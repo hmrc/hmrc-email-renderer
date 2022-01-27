c1cac608a637ed7850b009d12af4e7ac24be2eb7
Priyanka Dhiman <88194246+priyankadhiman3006@users.noreply.github.com>
2022-01-27 15:12:14 +0000

    ESC-265 email templates

--- app/uk/gov/hmrc/hmrcemailrenderer/templates/euSubsidy/EUSubsidyTemplates.scala
+++ app/uk/gov/hmrc/hmrcemailrenderer/templates/euSubsidy/EUSubsidyTemplates.scala
@@ -23,30 +23,102 @@ import uk.gov.hmrc.hmrcemailrenderer.templates.ServiceIdentifier.EUSubsidy
 object EUSubsidyTemplates {
   val templates = Seq(
     MessageTemplate.create(
-      templateId = "create_undertaking_by_lead",
+      templateId = "create_undertaking_email_to_lead",
       fromAddress = govUkTeamAddress,
       service = EUSubsidy,
       subject = "You created a new single undertaking",
-      plainTemplate = txt.createUndertakingByLead.f,
-      htmlTemplate = html.createUndertakingByLead.f,
+      plainTemplate = txt.createUndertakingEmailToLead.f,
+      htmlTemplate = html.createUndertakingEmailToLead.f,
       priority = Some(MessagePriority.Urgent)
     ),
     MessageTemplate.create(
-      templateId = "undertaking_member_confirmation",
+      templateId = "undertaking_member_added_email_to_be",
       fromAddress = govUkTeamAddress,
       service = EUSubsidy,
       subject = "You are now a member of a single undertaking",
-      plainTemplate = txt.undertakingMemberConfirmation.f,
-      htmlTemplate = html.undertakingMemberConfirmation.f,
+      plainTemplate = txt.undertakingMemberAddedEmailToBE.f,
+      htmlTemplate = html.undertakingMemberAddedEmailToBE.f,
       priority = Some(MessagePriority.Urgent)
     ),
     MessageTemplate.create(
-      templateId = "undertaking_member_added_by_lead",
+      templateId = "undertaking_member_added_email_to_lead",
       fromAddress = govUkTeamAddress,
       service = EUSubsidy,
-      subject = "You have been added to a single undertaking ",
-      plainTemplate = txt.undertakingMemberAddedByLead.f,
-      htmlTemplate = html.undertakingMemberAddedByLead.f,
+      subject = "You have added a member to your single undertaking",
+      plainTemplate = txt.undertakingMemberAddedEmailToLead.f,
+      htmlTemplate = html.undertakingMemberAddedEmailToLead.f,
+      priority = Some(MessagePriority.Urgent)
+    ),
+    MessageTemplate.create(
+      templateId = "undertaking_member_removed_email_to_lead",
+      fromAddress = govUkTeamAddress,
+      service = EUSubsidy,
+      subject = "You have removed a member from your single undertaking",
+      plainTemplate = txt.undertakingMemberRemovedEmailToLead.f,
+      htmlTemplate = html.undertakingMemberRemovedEmailToLead.f,
+      priority = Some(MessagePriority.Urgent)
+    ),
+    MessageTemplate.create(
+      templateId = "undertaking_member_removed_email_to_be",
+      fromAddress = govUkTeamAddress,
+      service = EUSubsidy,
+      subject = "You are removed from a single undertaking ",
+      plainTemplate = txt.undertakingMemberRemovedEmailToBE.f,
+      htmlTemplate = html.undertakingMemberRemovedEmailToBE.f,
+      priority = Some(MessagePriority.Urgent)
+    ),
+    MessageTemplate.create(
+      templateId = "promoted_other_as_lead_email_to_be",
+      fromAddress = govUkTeamAddress,
+      service = EUSubsidy,
+      subject = "You are now undertaking administrator ",
+      plainTemplate = txt.promotedOtherAsLeadEmailToBE.f,
+      htmlTemplate = html.promotedOtherAsLeadEmailToBE.f,
+      priority = Some(MessagePriority.Urgent)
+    ),
+    MessageTemplate.create(
+      templateId = "promoted_other_as_lead_email_to_lead",
+      fromAddress = govUkTeamAddress,
+      service = EUSubsidy,
+      subject = "Your status has changed and you are now an undertaking member",
+      plainTemplate = txt.promotedOtherAsLeadEmailToLead.f,
+      htmlTemplate = html.promotedOtherAsLeadEmailToLead.f,
+      priority = Some(MessagePriority.Urgent)
+    ),
+    MessageTemplate.create(
+      templateId = "member_remove_themself_email_to_lead",
+      fromAddress = govUkTeamAddress,
+      service = EUSubsidy,
+      subject = "You have removed yourself from a single undertaking",
+      plainTemplate = txt.memberRemoveThemselfEmailToLead.f,
+      htmlTemplate = html.memberRemoveThemselfEmailToLead.f,
+      priority = Some(MessagePriority.Urgent)
+    ),
+    MessageTemplate.create(
+      templateId = "member_remove_themself_email_to_be",
+      fromAddress = govUkTeamAddress,
+      service = EUSubsidy,
+      subject = "You have asked to leave a single undertaking",
+      plainTemplate = txt.memberRemoveThemselfEmailToBE.f,
+      htmlTemplate = html.memberRemoveThemselfEmailToBE.f,
+      priority = Some(MessagePriority.Urgent)
+    ),
+    MessageTemplate.create(
+      templateId = "promoted_themself_as_lead_email_to_lead",
+      fromAddress = govUkTeamAddress,
+      service = EUSubsidy,
+      subject = "You are now undertaking administrator",
+      plainTemplate = txt.promotedThemselfAsLeadEmailToLead.f,
+      htmlTemplate = html.promotedThemselfAsLeadEmailToLead.f,
+      priority = Some(MessagePriority.Urgent)
+    ),
+    MessageTemplate.create(
+      templateId = "removed_as_lead_email_to_previous_lead",
+      fromAddress = govUkTeamAddress,
+      service = EUSubsidy,
+      subject = "You have changed from undertaking member to undertaking administrator",
+      plainTemplate = txt.removedAsLeadEmailToPreviousLead.f,
+      htmlTemplate = html.removedAsLeadEmailToPreviousLead.f,
       priority = Some(MessagePriority.Urgent)
     )
   )
