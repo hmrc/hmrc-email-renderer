ce2d8542215f754432360abc89615a0d6c6ec20e
Matty Harris <matthew.harris@digital.hmrc.gov.uk>
2022-03-08 09:05:21 +0000

    DAC6-1468 | Added spacing to text and content change

--- app/uk/gov/hmrc/hmrcemailrenderer/templates/mdr/MdrTemplates.scala
+++ app/uk/gov/hmrc/hmrcemailrenderer/templates/mdr/MdrTemplates.scala
@@ -46,7 +46,7 @@ object MdrTemplates {
       templateId = "mdr_file_upload_successful",
       fromAddress = FromAddress.noReply("MDR@notifications.service.gov.uk"),
       service = MDR,
-      subject = "File has passed MDR checks",
+      subject = "Your file has passed MDR checks",
       plainTemplate = txt.mdrFileUploadSuccessful.f,
       htmlTemplate = html.mdrFileUploadSuccessful.f,
       priority = Some(MessagePriority.Urgent)
@@ -55,7 +55,7 @@ object MdrTemplates {
       templateId = "mdr_file_upload_unsuccessful",
       fromAddress = FromAddress.noReply("MDR@notifications.service.gov.uk"),
       service = MDR,
-      subject = "File has failed MDR checks",
+      subject = "Your file has failed MDR checks",
       plainTemplate = txt.mdrFileUploadUnsuccessful.f,
       htmlTemplate = html.mdrFileUploadUnsuccessful.f,
       priority = Some(MessagePriority.Urgent)
