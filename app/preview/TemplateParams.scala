a5ae320768974f8e507152ec9fc51d126715ca84
Jonathan-Courage <jonathan.courage@digital.hmrc.gov.uk>
2022-03-15 12:28:00 +0000

    DC-4125 AEO 117 and 118 Dynamic improvements (#868)
    * DC-4125 AEO 117 and 118 Dynamic improvements
    
    * Content feedback - Customer names paramatised
    
    * Content feedback from JIRA

--- app/preview/TemplateParams.scala
+++ app/preview/TemplateParams.scala
@@ -2717,6 +2717,16 @@ object TemplateParams2 {
       "para7"               -> "You can still use your URN to make further additions.",
       "para8"               -> "Select ‘start now’ from the GOV.UK page you used to start your original application. You can then confirm your email, then select ‘Make additions to an already approved application’."
     ),
+    "c117_application_confirmation" -> Map(
+      "customerName"        -> "Hugh Mann",
+      "submissionReference" -> "NC38-N2ZC-TH68"
+    ),
+    "c118_confirmation_dynamic" -> Map(
+      "customerName"        -> "Hugh Mann",
+      "c118DySub"           -> "HMRC received part of your AEO application",
+      "c118Section"         -> "C118 Section 1 Part 1",
+      "submissionReference" -> "NC38-N2ZC-TH68"
+    ),
     "jrsRemoval_submission_confirmation" -> Map(
       "customerName"        -> "Hugh Mann",
       "submissionReference" -> "NC38-N2ZC-TH68"
