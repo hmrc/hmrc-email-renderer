0a67717d76ec19ed67ae5df381ca2ba5b85268da
Jonathan-Courage <jonathan.courage@digital.hmrc.gov.uk>
2022-03-24 09:38:15 +0000

    Fix to rogue comma

--- app/preview/TemplateParams.scala
+++ app/preview/TemplateParams.scala
@@ -2882,7 +2882,7 @@ object TemplateParams2 {
     "utt_confirmation" -> Map(
       "customerName"        -> "Hugh Mann",
       "submissionReference" -> "NC38-N2ZC-TH68",
-      "companyName"         -> "Acquisitions Incorporated",
+      "companyName"         -> "Acquisitions Incorporated"
     ),
     "utt_code" -> Map(
       "confirmationCode" -> "HGDY"
