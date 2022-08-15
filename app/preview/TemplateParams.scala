50b262a9bc639b21901f01e7a8f915be08b63a5e
Jonathan-Courage <jcourage@live.co.uk>
2022-08-15 15:58:15 +0100

    DC-4416 Venture capital email template (#925)
    * DC-4416 Venture capital email template
    
    * Rogue comma, sbt fmt fix

--- app/preview/TemplateParams.scala
+++ app/preview/TemplateParams.scala
@@ -2921,6 +2921,15 @@ object TemplateParams2 {
       "customerName"        -> "Hugh Mann",
       "submissionReference" -> "NC38-N2ZC-TH68"
     ),
+    "venture_capital_code" -> Map(
+      "confirmationCode" -> "HGDY"
+    ),
+    "venture_capital_confirmation" -> Map(
+      "customerName"        -> "Hugh Mann",
+      "businessName"        -> "Acquisitions Incorporated",
+      "captionExpression"   -> "SEIS, EIS and VCT",
+      "submissionReference" -> "NC38-N2ZC-TH68"
+    ),
     "newMessageAlert_P800"        -> newMessageAlert_Names,
     "newMessageAlert_P800_cy"     -> newMessageAlert_Names,
     "newMessageAlert_PA302"       -> newMessageAlert_Names,
