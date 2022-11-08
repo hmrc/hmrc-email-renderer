b03b87047629a9925d7c52d27bdafcbdebdd591e
muralishub <17339891+muralishub@users.noreply.github.com>
2022-11-08 17:49:51 +0000

    DC-split: Fix  Method too large error in jenkins

--- app/preview/TemplateParams.scala
+++ app/preview/TemplateParams.scala
@@ -123,13 +123,6 @@ object TemplateParams {
     "agent_permissions_all_failed_cy" -> Map(
       "agencyName" -> "Agency"
     ),
-    "agent_permissions_beta_participant_details" -> Map(
-      "arn"             -> "TARN0000001",
-      "numClients"      -> "103",
-      "contactName"     -> "Bob De Builder",
-      "emailAddress"    -> "bob@builders.com",
-      "telephoneNumber" -> "+44(0) 208 123 4567"
-    ),
     "apiDeveloperEmailVerification" -> Map(
       "verificationLink"  -> exampleLinkWithRandomId,
       "developerHubTitle" -> "Developer Hub Title"
@@ -3089,6 +3082,114 @@ object TemplateParams2 {
     "newMessageAlert_PA302_D2"    -> newMessageAlert_Names,
     "newMessageAlert_PA302_D2_cy" -> newMessageAlert_Names,
     "new_message_alert_itsa"      -> newMessageAlert_Names,
-    "new_message_alert_itsa_cy"   -> newMessageAlert_Names
-  ) ++ ExtendedTemplateParams.exampleParams
+    "new_message_alert_itsa_cy"   -> newMessageAlert_Names,
+    "oss_registration_confirmation" -> Map(
+      "recipientName_line1"  -> "Joe Bloggs",
+      "businessName"         -> "Test Business",
+      "periodOfFirstReturn"  -> "July to September 2021",
+      "firstDayOfNextPeriod" -> "1 October 2021",
+      "commencementDate"     -> "12 June 2021",
+      "redirectLink"         -> "http://test.com/blah-blah"
+    ),
+    "oss_returns_email_confirmation" -> Map(
+      "recipientName_line1" -> "Joe Bloggs",
+      "businessName"        -> "Test Business",
+      "period"              -> "1 July to 30 September 2021",
+      "paymentDeadline"     -> "31 October 2021"
+    ),
+    "oss_returns_email_confirmation_no_vat_owed" -> Map(
+      "recipientName_line1" -> "Joe Bloggs",
+      "period"              -> "1 July to 30 September 2021"
+    ),
+    "oss_overdue_returns_email_confirmation" -> Map(
+      "recipientName_line1" -> "Joe Bloggs",
+      "businessName"        -> "Test Business",
+      "period"              -> "1 July to 30 September 2021",
+      "paymentDeadline"     -> "31 October 2021"
+    ),
+    "pods_psa_amend" -> Map(
+      "psaName" -> "Jane Doe"
+    ),
+    "hec_tax_check_code" -> Map(
+      "currentDate"     -> "13 January 2022",
+      "licenceType"     -> "Driver of taxis and private hires",
+      "hecTaxCheckCode" -> "FK9 BRG 2JJ",
+      "expiresAfter"    -> "13 May 2022"
+    ),
+    "hec_tax_check_code_cy" -> Map(
+      "currentDate"     -> "13 Ionawr 2022",
+      "licenceType"     -> "Gyrrwr tacsis a cherbydau hurio preifat",
+      "hecTaxCheckCode" -> "FK9 BRG 2JJ",
+      "expiresAfter"    -> "13 Mai 2022"
+    ),
+    "create_undertaking_email_to_lead" -> Map(
+      "eori"            -> "GB123456789012",
+      "undertakingName" -> "Test Undertaking"
+    ),
+    "undertaking_member_added_email_to_be" -> Map(
+      "eori"            -> "GB123456789010",
+      "undertakingName" -> "Test Undertaking"
+    ),
+    "undertaking_member_added_email_to_lead" -> Map(
+      "eori"            -> "GB123456789012",
+      "beEORI"          -> "GB123456789010",
+      "undertakingName" -> "Test Undertaking"
+    ),
+    "undertaking_member_removed_email_to_lead" -> Map(
+      "eori"            -> "GB123456789012",
+      "beEORI"          -> "GB123456789010",
+      "undertakingName" -> "Test Undertaking",
+      "effectiveDate"   -> "27 January 2022"
+    ),
+    "undertaking_member_removed_email_to_be" -> Map(
+      "eori"            -> "GB123456789012",
+      "undertakingName" -> "Test Undertaking",
+      "effectiveDate"   -> "27 January 2022"
+    ),
+    "promoted_other_as_lead_email_to_be" -> Map(
+      "eori"            -> "GB123456789010",
+      "undertakingName" -> "Test Undertaking"
+    ),
+    "promoted_other_as_lead_email_to_lead" -> Map(
+      "eori"            -> "GB123456789012",
+      "beEORI"          -> "GB123456789010",
+      "undertakingName" -> "Test Undertaking"
+    ),
+    "member_remove_themself_email_to_lead" -> Map(
+      "eori"            -> "GB123456789012",
+      "undertakingName" -> "Test Undertaking",
+      "effectiveDate"   -> "27 January 2022"
+    ),
+    "member_remove_themself_email_to_be" -> Map(
+      "eori"            -> "GB123456789010",
+      "undertakingName" -> "Test Undertaking",
+      "effectiveDate"   -> "27 January 2022"
+    ),
+    "promoted_themself_as_lead_email_to_lead" -> Map(
+      "eori"            -> "GB123456789010",
+      "undertakingName" -> "Test Undertaking",
+      "undertakingRef"  -> "UID0001234"
+    ),
+    "removed_as_lead_email_to_previous_lead" -> Map(
+      "eori"            -> "GB123456789010",
+      "undertakingName" -> "Test Undertaking"
+    ),
+    "disabled_undertaking_email_to_lead" -> Map(
+      "eori"            -> "GB123456789010",
+      "undertakingName" -> "Test Undertaking",
+      "effectiveDate"   -> "27 January 2022"
+    ),
+    "disabled_undertaking_email_to_be" -> Map(
+      "eori"            -> "GB123456789010",
+      "undertakingName" -> "Test Undertaking",
+      "effectiveDate"   -> "27 January 2022"
+    ),
+    "agent_permissions_beta_participant_details" -> Map(
+      "arn"             -> "TARN0000001",
+      "numClients"      -> "103",
+      "contactName"     -> "Bob De Builder",
+      "emailAddress"    -> "bob@builders.com",
+      "telephoneNumber" -> "+44(0) 208 123 4567"
+    )
+  )
 }
