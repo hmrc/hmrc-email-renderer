afaf24de57e08998590d33e94b8cd675c0e4098b
jonathan-courage <jcourage@live.co.uk>
2022-12-15 15:11:03 +0000

    Issue with compile

--- app/preview/TemplateParams.scala
+++ app/preview/TemplateParams.scala
@@ -3113,7 +3113,7 @@ object TemplateParams2 {
       "confirmationCode" -> "HGDY"
     ),
     "essDisclosure_confirmation" -> Map(
-      "customerName"        -> "Hugh Man",
+      "customerName"        -> "Hugh Mann",
       "submissionReference" -> "NC38-N2ZC-TH68"
     ),
     "rndNotification_code" -> Map(
