b313b8e3fa63836cbeb7af030874b8339ec34466
Jake Goodwin <12535866+JakeGoodwin@users.noreply.github.com>
2023-01-06 13:20:56 +0000

    ECC-1400: Added ECC Registration success templates

--- app/preview/TemplateParams.scala
+++ app/preview/TemplateParams.scala
@@ -2414,6 +2414,16 @@ object TemplateParams2 {
       "serviceName"            -> "Advance Tariff Rulings",
       "completionDate"         -> "22 March 2019"
     ),
+    "ecc_registration_successful" -> Map(
+      "recipientName_FullName" -> "Jane Jones",
+      "serviceName"            -> "Advance Tariff Rulings",
+      "enrolmentKey"           -> "HMRC-ATAR-ORG"
+    ),
+    "ecc_registration_successful_cy" -> Map(
+      "recipientName_FullName" -> "Jane Jones",
+      "serviceName"            -> "Advance Tariff Rulings",
+      "enrolmentKey"           -> "HMRC-ATAR-ORG"
+    ),
     "ecc_subscription_not_successful" -> Map(
       "recipientName_FullName" -> "Jane Jones",
       "recipientOrgName"       -> "JJ Components",
