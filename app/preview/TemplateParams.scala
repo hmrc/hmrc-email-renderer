c98eefc74b79b9f1bcd5de542e6fa91dfadcca60
Andy Hicks <455080+andy1138@users.noreply.github.com>
2023-02-02 09:35:07 +0000

    [OPS-9729] Welsh Payments DD Setup templates

--- app/preview/TemplateParams.scala
+++ app/preview/TemplateParams.scala
@@ -1862,29 +1862,54 @@ object TemplateParams2 {
       "recipientName_line1" -> "Colm Cavanagh"
     ),
     "sdds_ddi_setup_dcs_alert" -> Map(
-      "subject"             -> "subject",
+      "subject"             -> "regime Direct Debit Setup",
       "recipientName_line1" -> "Joe Smith",
       "sdds_regimeName"     -> "Soft Drinks Industry Levy"
+    ),
+    "sdds_ddi_setup_dcs_alert_cy" -> Map(
+      "subject" -> "regime Direct Debit Setup(Welsh)",
+      "recipientName_line1" -> "Cadfael Jones",
+      "sdds_regimeName" -> "Ardoll y Diwydiant Diodydd Ysgafn"
     ),
     "sdds_ddi_amended_dcs_alert" -> Map(
-      "subject"             -> "subject",
+      "subject"             -> "regime Direct Debit Amended",
       "recipientName_line1" -> "Joe Smith",
       "sdds_regimeName"     -> "Soft Drinks Industry Levy"
+    ),
+    "sdds_ddi_amended_dcs_alert_cy" -> Map(
+      "subject" -> "regime Direct Debit Amended(Welsh)",
+      "recipientName_line1" -> "Cadfael Jones",
+      "sdds_regimeName" -> "Ardoll y Diwydiant Diodydd Ysgafn"
     ),
     "sdds_ddi_cancelled_dcs_alert" -> Map(
-      "subject"             -> "subject",
+      "subject"             -> "regime Direct Debit Cancelled",
       "recipientName_line1" -> "Joe Smith",
       "sdds_regimeName"     -> "Soft Drinks Industry Levy"
+    ),
+    "sdds_ddi_cancelled_dcs_alert_cy" -> Map(
+      "subject" -> "regime Direct Debit Cancelled(Welsh)",
+      "recipientName_line1" -> "Cadfael Jones",
+      "sdds_regimeName" -> "Ardoll y Diwydiant Diodydd Ysgafn"
     ),
     "sdds_ddi_reminder_dcs_alert" -> Map(
-      "subject"             -> "subject",
+      "subject"             -> "regime Direct Debit Reminder",
       "recipientName_line1" -> "Joe Smith",
       "sdds_regimeName"     -> "Soft Drinks Industry Levy"
     ),
+    "sdds_ddi_reminder_dcs_alert_cy" -> Map(
+      "subject" -> "regime Direct Debit Reminder(Welsh)",
+      "recipientName_line1" -> "Cadfael Jones",
+      "sdds_regimeName" -> "Ardoll y Diwydiant Diodydd Ysgafn"
+    ),
     "sdds_ddi_unpaid_dcs_alert" -> Map(
-      "subject"             -> "subject",
+      "subject"             -> "regime Direct Debit Unpaid",
       "recipientName_line1" -> "Joe Smith",
       "sdds_regimeName"     -> "Soft Drinks Industry Levy"
+    ),
+    "sdds_ddi_unpaid_dcs_alert_cy" -> Map(
+      "subject"             -> "regime Direct Debit Unpaid(Welsh)",
+      "recipientName_line1" -> "Cadfael Jones",
+      "sdds_regimeName"     -> "Ardoll y Diwydiant Diodydd Ysgafn"
     ),
     "dd_email_verifcation" -> Map(
       "name"             -> "Joe",
