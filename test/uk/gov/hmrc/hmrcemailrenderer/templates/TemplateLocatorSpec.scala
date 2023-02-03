95455921af70b267a00f4acf7fcc375c85356738
jonathan-courage <jcourage@live.co.uk>
2023-02-03 11:13:29 +0000

    DC-4746 HO5 Return email template

--- test/uk/gov/hmrc/hmrcemailrenderer/templates/TemplateLocatorSpec.scala
+++ test/uk/gov/hmrc/hmrcemailrenderer/templates/TemplateLocatorSpec.scala
@@ -457,6 +457,7 @@ class TemplateLocatorSpec extends AnyWordSpecLike with should.Matchers with Opti
         "seis_code",
         "ca3822_confirmation",
         "ca3822_code",
+        "ho5_confirmation",
         "trusts_report_code",
         "trusts_report_confirmation",
         "trusts_request_code",
