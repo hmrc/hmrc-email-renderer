32062f6af2aacf36e9fdfc3d4c4ea31aadfafb01
Satya Josyula <74477958+satyajosyula2020@users.noreply.github.com>
2023-05-31 08:21:54 +0100

    DC-4685 : Create ITSA Email Template for each ITSA Output (#1056)

--- app/uk/gov/hmrc/hmrcemailrenderer/templates/itsa/ItsaTemplates.scala
+++ app/uk/gov/hmrc/hmrcemailrenderer/templates/itsa/ItsaTemplates.scala
@@ -39,6 +39,258 @@ object ItsaTemplates {
       plainTemplate = txt.new_message_alert_itsa_cy.f,
       htmlTemplate = html.new_message_alert_itsa_cy.f,
       priority = Some(MessagePriority.Standard)
+    ),
+    MessageTemplate.create(
+      templateId = "new_message_alert_itsaqu1",
+      fromAddress = govUkTeamAddress,
+      service = Itsa,
+      subject = "You’ve got a new message from HMRC",
+      plainTemplate = txt.new_message_alert_itsaqu1.f,
+      htmlTemplate = html.new_message_alert_itsaqu1.f,
+      priority = Some(MessagePriority.Standard)
+    ),
+    MessageTemplate.create(
+      templateId = "new_message_alert_itsaqu1_cy",
+      fromAddress = govUkTeamAddress,
+      service = Itsa,
+      subject = "Mae gennych neges newydd oddi wrth CThEM",
+      plainTemplate = txt.new_message_alert_itsaqu1_cy.f,
+      htmlTemplate = html.new_message_alert_itsaqu1_cy.f,
+      priority = Some(MessagePriority.Standard)
+    ),
+    MessageTemplate.create(
+      templateId = "new_message_alert_itsaqu2",
+      fromAddress = govUkTeamAddress,
+      service = Itsa,
+      subject = "You’ve got a new message from HMRC",
+      plainTemplate = txt.new_message_alert_itsaqu2.f,
+      htmlTemplate = html.new_message_alert_itsaqu2.f,
+      priority = Some(MessagePriority.Standard)
+    ),
+    MessageTemplate.create(
+      templateId = "new_message_alert_itsaqu2_cy",
+      fromAddress = govUkTeamAddress,
+      service = Itsa,
+      subject = "Mae gennych neges newydd oddi wrth CThEM",
+      plainTemplate = txt.new_message_alert_itsaqu2_cy.f,
+      htmlTemplate = html.new_message_alert_itsaqu2_cy.f,
+      priority = Some(MessagePriority.Standard)
+    ),
+    MessageTemplate.create(
+      templateId = "new_message_alert_itsaeops1",
+      fromAddress = govUkTeamAddress,
+      service = Itsa,
+      subject = "You’ve got a new message from HMRC",
+      plainTemplate = txt.new_message_alert_itsaeops1.f,
+      htmlTemplate = html.new_message_alert_itsaeops1.f,
+      priority = Some(MessagePriority.Standard)
+    ),
+    MessageTemplate.create(
+      templateId = "new_message_alert_itsaeops1_cy",
+      fromAddress = govUkTeamAddress,
+      service = Itsa,
+      subject = "Mae gennych neges newydd oddi wrth CThEM",
+      plainTemplate = txt.new_message_alert_itsaeops1_cy.f,
+      htmlTemplate = html.new_message_alert_itsaeops1_cy.f,
+      priority = Some(MessagePriority.Standard)
+    ),
+    MessageTemplate.create(
+      templateId = "new_message_alert_itsaeops2",
+      fromAddress = govUkTeamAddress,
+      service = Itsa,
+      subject = "You’ve got a new message from HMRC",
+      plainTemplate = txt.new_message_alert_itsaeops2.f,
+      htmlTemplate = html.new_message_alert_itsaeops2.f,
+      priority = Some(MessagePriority.Standard)
+    ),
+    MessageTemplate.create(
+      templateId = "new_message_alert_itsaeops2_cy",
+      fromAddress = govUkTeamAddress,
+      service = Itsa,
+      subject = "Mae gennych neges newydd oddi wrth CThEM",
+      plainTemplate = txt.new_message_alert_itsaeops2_cy.f,
+      htmlTemplate = html.new_message_alert_itsaeops2_cy.f,
+      priority = Some(MessagePriority.Standard)
+    ),
+    MessageTemplate.create(
+      templateId = "new_message_alert_itsaeopsf",
+      fromAddress = govUkTeamAddress,
+      service = Itsa,
+      subject = "You’ve got a new message from HMRC",
+      plainTemplate = txt.new_message_alert_itsaeopsf.f,
+      htmlTemplate = html.new_message_alert_itsaeopsf.f,
+      priority = Some(MessagePriority.Standard)
+    ),
+    MessageTemplate.create(
+      templateId = "new_message_alert_itsaeopsf_cy",
+      fromAddress = govUkTeamAddress,
+      service = Itsa,
+      subject = "Mae gennych neges newydd oddi wrth CThEM",
+      plainTemplate = txt.new_message_alert_itsaeopsf_cy.f,
+      htmlTemplate = html.new_message_alert_itsaeopsf_cy.f,
+      priority = Some(MessagePriority.Standard)
+    ),
+    MessageTemplate.create(
+      templateId = "new_message_alert_itsapoa1-1",
+      fromAddress = govUkTeamAddress,
+      service = Itsa,
+      subject = "You’ve got a new message from HMRC",
+      plainTemplate = txt.new_message_alert_itsapoa1_1.f,
+      htmlTemplate = html.new_message_alert_itsapoa1_1.f,
+      priority = Some(MessagePriority.Standard)
+    ),
+    MessageTemplate.create(
+      templateId = "new_message_alert_itsapoa1-1_cy",
+      fromAddress = govUkTeamAddress,
+      service = Itsa,
+      subject = "Mae gennych neges newydd oddi wrth CThEM",
+      plainTemplate = txt.new_message_alert_itsapoa1_1_cy.f,
+      htmlTemplate = html.new_message_alert_itsapoa1_1_cy.f,
+      priority = Some(MessagePriority.Standard)
+    ),
+    MessageTemplate.create(
+      templateId = "new_message_alert_itsapoa1-2",
+      fromAddress = govUkTeamAddress,
+      service = Itsa,
+      subject = "You’ve got a new message from HMRC",
+      plainTemplate = txt.new_message_alert_itsapoa1_2.f,
+      htmlTemplate = html.new_message_alert_itsapoa1_2.f,
+      priority = Some(MessagePriority.Standard)
+    ),
+    MessageTemplate.create(
+      templateId = "new_message_alert_itsapoa1-2_cy",
+      fromAddress = govUkTeamAddress,
+      service = Itsa,
+      subject = "Mae gennych neges newydd oddi wrth CThEM",
+      plainTemplate = txt.new_message_alert_itsapoa1_2_cy.f,
+      htmlTemplate = html.new_message_alert_itsapoa1_2_cy.f,
+      priority = Some(MessagePriority.Standard)
+    ),
+    MessageTemplate.create(
+      templateId = "new_message_alert_itsapoa2-1",
+      fromAddress = govUkTeamAddress,
+      service = Itsa,
+      subject = "You’ve got a new message from HMRC",
+      plainTemplate = txt.new_message_alert_itsapoa2_1.f,
+      htmlTemplate = html.new_message_alert_itsapoa2_1.f,
+      priority = Some(MessagePriority.Standard)
+    ),
+    MessageTemplate.create(
+      templateId = "new_message_alert_itsapoa2-1_cy",
+      fromAddress = govUkTeamAddress,
+      service = Itsa,
+      subject = "Mae gennych neges newydd oddi wrth CThEM",
+      plainTemplate = txt.new_message_alert_itsapoa2_1_cy.f,
+      htmlTemplate = html.new_message_alert_itsapoa2_1_cy.f,
+      priority = Some(MessagePriority.Standard)
+    ),
+    MessageTemplate.create(
+      templateId = "new_message_alert_itsapoa2-2",
+      fromAddress = govUkTeamAddress,
+      service = Itsa,
+      subject = "You’ve got a new message from HMRC",
+      plainTemplate = txt.new_message_alert_itsapoa2_2.f,
+      htmlTemplate = html.new_message_alert_itsapoa2_2.f,
+      priority = Some(MessagePriority.Standard)
+    ),
+    MessageTemplate.create(
+      templateId = "new_message_alert_itsapoa2-2_cy",
+      fromAddress = govUkTeamAddress,
+      service = Itsa,
+      subject = "Mae gennych neges newydd oddi wrth CThEM",
+      plainTemplate = txt.new_message_alert_itsapoa2_2_cy.f,
+      htmlTemplate = html.new_message_alert_itsapoa2_2_cy.f,
+      priority = Some(MessagePriority.Standard)
+    ),
+    MessageTemplate.create(
+      templateId = "new_message_alert_itsafd1",
+      fromAddress = govUkTeamAddress,
+      service = Itsa,
+      subject = "You’ve got a new message from HMRC",
+      plainTemplate = txt.new_message_alert_itsafd1.f,
+      htmlTemplate = html.new_message_alert_itsafd1.f,
+      priority = Some(MessagePriority.Standard)
+    ),
+    MessageTemplate.create(
+      templateId = "new_message_alert_itsafd1_cy",
+      fromAddress = govUkTeamAddress,
+      service = Itsa,
+      subject = "Mae gennych neges newydd oddi wrth CThEM",
+      plainTemplate = txt.new_message_alert_itsafd1_cy.f,
+      htmlTemplate = html.new_message_alert_itsafd1_cy.f,
+      priority = Some(MessagePriority.Standard)
+    ),
+    MessageTemplate.create(
+      templateId = "new_message_alert_itsafd2",
+      fromAddress = govUkTeamAddress,
+      service = Itsa,
+      subject = "You’ve got a new message from HMRC",
+      plainTemplate = txt.new_message_alert_itsafd2.f,
+      htmlTemplate = html.new_message_alert_itsafd2.f,
+      priority = Some(MessagePriority.Standard)
+    ),
+    MessageTemplate.create(
+      templateId = "new_message_alert_itsafd2_cy",
+      fromAddress = govUkTeamAddress,
+      service = Itsa,
+      subject = "Mae gennych neges newydd oddi wrth CThEM",
+      plainTemplate = txt.new_message_alert_itsafd2_cy.f,
+      htmlTemplate = html.new_message_alert_itsafd2_cy.f,
+      priority = Some(MessagePriority.Standard)
+    ),
+    MessageTemplate.create(
+      templateId = "new_message_alert_itsafd3",
+      fromAddress = govUkTeamAddress,
+      service = Itsa,
+      subject = "You’ve got a new message from HMRC",
+      plainTemplate = txt.new_message_alert_itsafd3.f,
+      htmlTemplate = html.new_message_alert_itsafd3.f,
+      priority = Some(MessagePriority.Standard)
+    ),
+    MessageTemplate.create(
+      templateId = "new_message_alert_itsafd3_cy",
+      fromAddress = govUkTeamAddress,
+      service = Itsa,
+      subject = "Mae gennych neges newydd oddi wrth CThEM",
+      plainTemplate = txt.new_message_alert_itsafd3_cy.f,
+      htmlTemplate = html.new_message_alert_itsafd3_cy.f,
+      priority = Some(MessagePriority.Standard)
+    ),
+    MessageTemplate.create(
+      templateId = "new_message_alert_itsapoa-cn",
+      fromAddress = govUkTeamAddress,
+      service = Itsa,
+      subject = "You’ve got a new message from HMRC",
+      plainTemplate = txt.new_message_alert_itsapoa_cn.f,
+      htmlTemplate = html.new_message_alert_itsapoa_cn.f,
+      priority = Some(MessagePriority.Standard)
+    ),
+    MessageTemplate.create(
+      templateId = "new_message_alert_itsapoa-cn_cy",
+      fromAddress = govUkTeamAddress,
+      service = Itsa,
+      subject = "Mae gennych neges newydd oddi wrth CThEM",
+      plainTemplate = txt.new_message_alert_itsapoa_cn_cy.f,
+      htmlTemplate = html.new_message_alert_itsapoa_cn.f,
+      priority = Some(MessagePriority.Standard)
+    ),
+    MessageTemplate.create(
+      templateId = "new_message_alert_itsauc1",
+      fromAddress = govUkTeamAddress,
+      service = Itsa,
+      subject = "You’ve got a new message from HMRC",
+      plainTemplate = txt.new_message_alert_itsauc1.f,
+      htmlTemplate = html.new_message_alert_itsafd1.f,
+      priority = Some(MessagePriority.Standard)
+    ),
+    MessageTemplate.create(
+      templateId = "new_message_alert_itsauc1_cy",
+      fromAddress = govUkTeamAddress,
+      service = Itsa,
+      subject = "Mae gennych neges newydd oddi wrth CThEM",
+      plainTemplate = txt.new_message_alert_itsauc1_cy.f,
+      htmlTemplate = html.new_message_alert_itsauc1_cy.f,
+      priority = Some(MessagePriority.Standard)
     )
   )
 }
