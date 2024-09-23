58efa79a3786f230b0ef3db50ce0c2d8d80247d2
besscerule <63503574+besscerule@users.noreply.github.com>
2024-09-23 14:47:37 +0100

    linting again

--- app/uk/gov/hmrc/hmrcemailrenderer/templates/tgp/TgpTemplates.scala
+++ app/uk/gov/hmrc/hmrcemailrenderer/templates/tgp/TgpTemplates.scala
@@ -16,7 +16,7 @@
 
 package uk.gov.hmrc.hmrcemailrenderer.templates.tgp
 
-import uk.gov.hmrc.hmrcemailrenderer.domain.{MessagePriority, MessageTemplate}
+import uk.gov.hmrc.hmrcemailrenderer.domain.{ MessagePriority, MessageTemplate }
 import uk.gov.hmrc.hmrcemailrenderer.templates.FromAddress
 import uk.gov.hmrc.hmrcemailrenderer.templates.ServiceIdentifier.Tgp
 
