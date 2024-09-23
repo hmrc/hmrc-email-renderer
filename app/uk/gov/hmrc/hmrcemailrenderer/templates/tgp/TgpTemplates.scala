8c1ca20d760df7f8b878ad3b8f47d9148a15293a
besscerule <63503574+besscerule@users.noreply.github.com>
2024-09-23 13:28:05 +0100

    Added email

---
+++ app/uk/gov/hmrc/hmrcemailrenderer/templates/tgp/TgpTemplates.scala
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package tgp
+
+import uk.gov.hmrc.hmrcemailrenderer.domain.{MessagePriority, MessageTemplate}
+import uk.gov.hmrc.hmrcemailrenderer.templates.FromAddress
+import uk.gov.hmrc.hmrcemailrenderer.templates.ServiceIdentifier.Tgp
+
+object TgpTemplates {
+
+  val templates = Seq(
+    MessageTemplate.create(
+      templateId = "tgp_download_record_notification_email",
+      fromAddress = FromAddress.noReply("Do not reply"),
+      service = Tgp,
+      subject = "Your TGP records file is ready to download",
+      plainTemplate = txt.DownloadRecordNotification.f,
+      htmlTemplate = html.DownloadRecordNotification.f,
+      priority = Some(MessagePriority.Urgent)
+    ),
+    MessageTemplate.create(
+      templateId = "tgp_download_record_notification_email_cy",
+      fromAddress = FromAddress.noReply("Do not reply"),
+      service = Tgp,
+      subject = "Mae’r ffeil sy’n cynnwys cofnodion eich TGP yn barod i’w lawrlwytho",
+      plainTemplate = txt.DownloadRecordNotification_cy.f,
+      htmlTemplate = html.DownloadRecordNotification_cy.f,
+      priority = Some(MessagePriority.Urgent)
+    )
+  )
+}
