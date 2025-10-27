903ea822e41775c4afd59aea2cde9d1b9856b815
tyagikumarvineet <22053543+tyagikumarvineet@users.noreply.github.com>
2025-10-27 23:12:19 +0000

    [DC-7958] Remove redundant dependencies that are coming from bootstrap

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,16 +6,13 @@ private object AppDependencies {
 
   def apply(): Seq[ModuleID] = Seq(
     ws,
-    "uk.gov.hmrc"            %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc"            %% "domain-play-30"             % "13.0.0",
-    "net.codingwell"         %% "scala-guice"                % "6.0.0",
-    "org.jsoup"               % "jsoup"                      % "1.13.1"         % Test,
-    "uk.gov.hmrc"            %% "bootstrap-test-play-30"     % bootstrapVersion % Test,
-    "org.scalatestplus.play" %% "scalatestplus-play"         % "7.0.1"          % Test,
-    "org.mockito"             % "mockito-core"               % "4.7.0"          % Test,
-    "org.scalatestplus"      %% "mockito-4-11"               % "3.2.17.0"       % Test,
-    "com.github.tomakehurst"  % "wiremock"                   % "2.27.2"         % Test,
-    "com.vladsch.flexmark"    % "flexmark-all"               % "0.35.10"        % Test
+    "uk.gov.hmrc"           %% "bootstrap-frontend-play-30" % bootstrapVersion,
+    "uk.gov.hmrc"           %% "domain-play-30"             % "13.0.0",
+    "net.codingwell"        %% "scala-guice"                % "6.0.0",
+    "org.jsoup"              % "jsoup"                      % "1.13.1"         % Test,
+    "uk.gov.hmrc"           %% "bootstrap-test-play-30"     % bootstrapVersion % Test,
+    "org.mockito"            % "mockito-core"               % "4.7.0"          % Test,
+    "com.github.tomakehurst" % "wiremock"                   % "2.27.2"         % Test
   )
 
   val it: Seq[ModuleID] = Seq(
