92ba1536edde3c180fc31fb9c98a4e6587e60c7e
kevin-co-hmrc <129957464+kevin-co-hmrc@users.noreply.github.com>
2026-07-10 15:13:56 +0100

    VTCCA-8339 Replace 'Valuation Office Agency' with 'Valuation Office' (#1321)
    * VTCCA-8339 Replace 'Valuation Office Agency' with 'Valuation Office'
    
    * Add EOF newlines
    
    * Fix test
    
    * Remove revaluation email template
    
    * Fix typo in plain text template

--- app/uk/gov/hmrc/hmrcemailrenderer/templates/voa/CCATemplates.scala
+++ app/uk/gov/hmrc/hmrcemailrenderer/templates/voa/CCATemplates.scala
@@ -22,15 +22,12 @@ import uk.gov.hmrc.hmrcemailrenderer.templates.ServiceIdentifier.CCA
 import registration._
 object CCATemplates {
 
-  private def appealsSubject(params: Map[String, String]) =
-    s"EIPP ${params.getOrElse("submissionDate", "")} ${params.getOrElse("postcode", "")}"
-
   val templates = Seq(
     MessageTemplate.create(
       templateId = "cca_enrolment_migration_confirmation",
       fromAddress = govUkTeamAddress,
       service = CCA,
-      subject = "Valuation Office Agency - check and challenge registration",
+      subject = "Valuation Office - check and challenge registration",
       plainTemplate = txt.ccaEnrolmentConfirmationExisting.f,
       htmlTemplate = html.ccaEnrolmentConfirmationExisting.f,
       priority = Some(MessagePriority.Standard)
@@ -39,7 +36,7 @@ object CCATemplates {
       templateId = "cca_enrolment_confirmation",
       fromAddress = govUkTeamAddress,
       service = CCA,
-      subject = "Valuation Office Agency - check and challenge registration",
+      subject = "Valuation Office - check and challenge registration",
       plainTemplate = registration.txt.ccaEnrolmentConfirmation.f,
       htmlTemplate = registration.html.ccaEnrolmentConfirmation.f,
       priority = Some(MessagePriority.Standard)
@@ -48,7 +45,7 @@ object CCATemplates {
       templateId = "cca_enrolment_confirmation_agent",
       fromAddress = govUkTeamAddress,
       service = CCA,
-      subject = "Valuation Office Agency - check and challenge registration",
+      subject = "Valuation Office - check and challenge registration",
       plainTemplate = registration.txt.ccaEnrolmentConfirmationAgent.f,
       htmlTemplate = registration.html.ccaEnrolmentConfirmationAgent.f,
       priority = Some(MessagePriority.Standard)
@@ -57,27 +54,9 @@ object CCATemplates {
       templateId = "cca_enrolment_confirmation_individual",
       fromAddress = govUkTeamAddress,
       service = CCA,
-      subject = "Valuation Office Agency - check and challenge registration",
+      subject = "Valuation Office - check and challenge registration",
       plainTemplate = registration.txt.ccaEnrolmentConfirmationIndividual.f,
       htmlTemplate = registration.html.ccaEnrolmentConfirmationIndividual.f,
-      priority = Some(MessagePriority.Standard)
-    ),
-    MessageTemplate.create(
-      templateId = "cca_revaluation_subscription_confirmation",
-      fromAddress = govUkTeamAddress,
-      service = CCA,
-      subject = "You signed up for news about business rate revaluations",
-      plainTemplate = subscription.txt.ccaRevalSubscriptionConfirmation.f,
-      htmlTemplate = subscription.html.ccaRevalSubscriptionConfirmation.f,
-      priority = Some(MessagePriority.Standard)
-    ),
-    MessageTemplate.createWithDynamicSubject(
-      templateId = "cca_appeals_submission_link",
-      fromAddress = govUkTeamAddress,
-      service = CCA,
-      subject = appealsSubject(_),
-      plainTemplate = appeals.txt.ccaAppealsSubmissionData.f,
-      htmlTemplate = appeals.html.ccaAppealsSubmissionData.f,
       priority = Some(MessagePriority.Standard)
     )
   )
